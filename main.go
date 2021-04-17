88f05d50513a16b089f2c4c47b7593e8ec7a8bbb
Shivam <shivam@reingames.com>
2021-04-17 21:46:14 +0530

    Modified Readme

--- main.go
+++ main.go
@@ -5,24 +5,63 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
+//Read the problems in CSV format from file path
+func getProblemsFrom(fileName string) ([]problem, error) {
+	if fObj, err := os.Open(fileName); err == nil {
+		csvR := csv.NewReader(fObj)
+		if cLines, err := csvR.ReadAll(); err == nil {
+			return parseProblem(cLines), nil
+		} else {
+			return nil, fmt.Errorf("error in reading data in CSV "+
+				"format from %s file; %s", fileName, err.Error())
+		}
+	} else {
+		return nil, fmt.Errorf("error in opening %s file; %s", fileName, err.Error())
+	}
+}
 func main() {
 	fName := flag.String("f", "default.csv", "path of csv file")
+	timer := flag.Int("t", 30, "timer of the quiz")
 	flag.Parse()
 
-	fObj, err := os.Open(*fName)
+	problems, err := getProblemsFrom(*fName)
 	if err != nil {
-		exit(fmt.Sprintf("error occured in open %s", *fName))
+		exit(fmt.Sprintf("something wrong happened exiting program: %s", err.Error()))
 	}
-	csvR := csv.NewReader(fObj)
-	cLines, err := csvR.ReadAll()
-	if err != nil {
-		exit(fmt.Sprintf("error in reading all line from %s file", *fName))
-	}
-	problems := parseProblem(cLines)
-	fmt.Println(problems)
+	correctAns := 0
+	tObj := time.NewTimer(time.Duration(*timer) * time.Second)
+	ansC := make(chan string)
 
+problemLoop:
+	for i, p := range problems {
+		var answer string
+
+		fmt.Printf("Problem %d: %s=", i+1, p.q)
+
+		go func() {
+			fmt.Scanf("%s", &answer)
+			ansC <- answer
+		}()
+		select {
+		case <-tObj.C:
+			fmt.Println()
+			break problemLoop
+		case iAns := <-ansC:
+			if iAns == p.a {
+				correctAns++
+			}
+			//If user has answered all questions then close the answer channel
+			if i == len(problems)-1 {
+				close(ansC)
+			}
+		}
+	}
+	fmt.Printf("Your result %d out of %d\n", correctAns, len(problems))
+	fmt.Printf("Press Enter to quit...")
+	<-ansC
 }
 
 func parseProblem(lines [][]string) []problem {
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 Quiz application developed in go
+
+1. Here I have used channels to control the time out of the quiz round.
+2. CSV file is used.
